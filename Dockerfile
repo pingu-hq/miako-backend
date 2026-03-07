0453b4bebd5a9274059204bbbaf15bf4a6b57994
Nana Mein <alfeoephraimquintay@gmail.com>
2026-03-08 02:48:58 +0800

    Chore: Fix docker because it cant run without uvicorn. Added cors in main.py

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,5 @@ COPY . /app
 
 RUN uv sync --frozen --no-cache
 
-CMD ["/app/.venv/bin/fastapi", "run", "main.py", "--port", "80", "--host", "0.0.0.0"]
+#CMD ["/app/.venv/bin/fastapi", "run", "main:app", "--port", "80", "--host", "0.0.0.0"]
+CMD ["/app/.venv/bin/uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from core.config import settings
 from routers.llm_workflow_router import router as llm_router
 
 
 app = FastAPI()
+
+# --- CORS Configuration ---
+# U-um... This allows requests from any domain...
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# --------------------------
+
 app.include_router(llm_router)
 
-
-if __name__ == "__main__":
-    import uvicorn
-    print("RUNNING UVICORN")
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True
-    )
+# if __name__ == "__main__":
+#     import uvicorn
+#     print("RUNNING UVICORN")
+#     uvicorn.run(
+#         "main:app",
+#         host="0.0.0.0",
+#         port=8000,
+#         reload=True
+#     )
