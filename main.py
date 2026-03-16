0d0845a21c10fc1a0670abb3f07e5d225612ef93
Nana Mein <alfeoephraimquintay@gmail.com>
2026-03-16 18:55:09 +0800

    Chore: Added Origins and with Made it so that in one env var, we can add multiple or single domain needed for CORS configuration.

--- main.py
+++ main.py
@@ -8,15 +8,20 @@ from core.config import settings
 app = FastAPI(
     docs_url="/api-docs",
     redoc_url="/api-redoc",
-    openapi_url="/openapi.json",
+    openapi_url="/api-openapi.json",
     title="Miako Hackathon Project",
 )
 
 
-domain = [settings.DOMAIN]
+# domain = [settings.DOMAIN]
+origins = settings.ORIGINS.get_secret_value()
+allow_origins = [
+    origin.strip()
+    for origin in origins.split(",")
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=domain,
+    allow_origins=allow_origins,
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["Authorization", "Content-Type"],
