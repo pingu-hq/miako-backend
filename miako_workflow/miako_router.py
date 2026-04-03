44b968a1f89ae34f32d21b05ff8f8723b6966680
Nana Mein <alfeoephraimquintay@gmail.com>
2026-04-03 15:45:29 +0800

    Feature: Added error handling logs and logger

--- miako_workflow/miako_router.py
+++ miako_workflow/miako_router.py
@@ -14,6 +14,7 @@ from databases.database import get_session
 from pydantic import BaseModel, Field
 from miako_workflow.workflows.steps.decision_step import DecisionStepAzure
 from miako_workflow.config_files.config import workflow_settings
+from core.app_logger import logger
 
 
 class UserBase(BaseModel):
@@ -51,11 +52,14 @@ async def send_message(request: MessageRequest, user_id = Depends(get_current_us
             input_message=request.message
         )
         response = await chat_obj.execute_agent()
+        logger.success(f"Send message successful for user: {user_id}")
         return MessageRequest(message=str(response))
-    except HTTPException:
+    except HTTPException as err1:
+        logger.error(f"Send message failed for user: {user_id}. Error message: {err1.detail}")
         raise
-    except Exception as err:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(err))
+    except Exception as err2:
+        logger.critical(f"Send message failed for user: {user_id}. Error message: {err2}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
 
 
 
@@ -72,29 +76,35 @@ async def sign_up_user(payload: UserCreate, session: AsyncSession = Depends(get_
         session.add(db_user)
         await session.commit()
         await session.refresh(db_user)
+        logger.success(f"Sign up successful. Email: {db_user.email}")
         return {"status": status.HTTP_201_CREATED}
     except Exception as err:
+        logger.error(f"Sign up failed for user: {payload.email}. Error message: {err}")
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(err))
 
 
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def login_user(payload: UserLogin, session: AsyncSession = Depends(get_session)):
     if not payload.email or not payload.password:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email and password is required")
+        logger.error("Email and password is required")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Bad Request")
 
     statement = select(User).where(User.email == payload.email)
     result = await session.execute(statement=statement)
     user = result.scalar_one_or_none()
 
-    error_401 = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password")
+    error_401 = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized User")
 
     if not user:
+        logger.error("Invalid email or password")
         raise error_401
 
     is_valid = await verify_hash_password(user.hashed_password, payload.password)
     if not is_valid:
+        logger.error("Invalid password")
         raise error_401
 
+    logger.success(f"Login successful. Email: {payload.email}")
     return login_response_tokens(subject=user.uuid)
 
 @router.post("/refresh", response_model=RefreshTokenResponse)
@@ -107,15 +117,19 @@ def me_test(payload: RefreshTokenRequest):
             access_token=new_access_token,
             token_type="bearer"
         )
-    except HTTPException:
+    except HTTPException as err1:
+        logger.success(f"Refresh token failed. Error message: {err1.detail}")
         raise
-    except Exception as err:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(err))
+    except Exception as err2:
+        logger.critical(f"Refresh token failed. Error message: {err2}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
 
 
 @router.get("/hello", status_code=status.HTTP_202_ACCEPTED)
 def hello_world():
     _hello = workflow_settings.HELLO_WORLD.upper()
     if _hello:
+        logger.success(f"Hello world successful. Message: {_hello}")
         return {"MESSAGE": _hello}
+    logger.warning(f"Hello world failed. Message: {_hello}")
     raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED)
